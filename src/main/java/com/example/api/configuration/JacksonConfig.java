fed4e1c5380b89b88665009ad5b83a1ad9ecf198
sk.kwon <sgpioneer@naver.com>
2023-01-04 23:44:05 +0900

    change objectmapper configure json include alwasys

--- src/main/java/com/example/api/configuration/JacksonConfig.java
+++ src/main/java/com/example/api/configuration/JacksonConfig.java
@@ -11,20 +11,12 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 /**
  * <pre>
  * ObjectMapper 및 POJO to JSON, JSON to POJO 시 적용할 config
  * </pre>
- * 
- * <p>
- * - null 제외
- * <p>
- * - Collection : isEmpty true 제외
- * <p>
- * - Array : size() == 0 제외
- * <p>
- * - String : length == 0 제외
  * 
  * @author sk.kwon
  */
@@ -34,13 +26,13 @@ public class JacksonConfig {
    * Json Serialize/Deserialize 설정
    * <p>
    * ObjectMapper 및 POJO to JSON, JSON to POJO
-   * 
+   *
    * @return Jackson2ObjectMapperBuilderCustomizer
    */
   @Bean
   public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
     return builder -> builder
-        .serializationInclusion(JsonInclude.Include.NON_EMPTY)
+        .serializationInclusion(JsonInclude.Include.ALWAYS)
         .featuresToDisable(
             SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
         .timeZone(TimeZone.getDefault())
